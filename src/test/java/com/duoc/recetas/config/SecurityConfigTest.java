e3a01302bf83b07709548324bacf94249a900039
DiegoMoralesDuoc <dieg.moralesa@duocuc.cl>
2025-12-15 20:12:51 -0300

    Se modifica SecurityConfigTest para aumentar su coverage

--- src/test/java/com/duoc/recetas/config/SecurityConfigTest.java
+++ src/test/java/com/duoc/recetas/config/SecurityConfigTest.java
@@ -14,7 +14,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 
 class SecurityConfigTest {
 
